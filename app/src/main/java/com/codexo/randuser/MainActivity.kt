a5c739af104a4c688bfd208693b0c000dbbc5a27
elysium09 <eyueldaniel0921@gmail.com>
2021-12-10 11:56:29 +0300

    :tada: init

---
+++ app/src/main/java/com/codexo/randuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codexo.randuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
